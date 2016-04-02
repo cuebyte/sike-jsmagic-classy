94773237cb2156802ae3847b9171988fcbaf4bd7
cuebyte <cuebit.w@gmail.com>
2016-04-02 12:04:04 +0800

    bugfix

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ function Class(child, parent) {
     child.__super__ = Object
   }
   child.prototype.__stack = child
-  child.prototype.__stack = child
   child.prototype.super = function(methodName) {
     this.__stack = this.__stack.__super__
     var result = this.__stack.prototype[methodName].apply(this, [].slice.call(arguments, 1))
